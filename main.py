466f5f3a37d0f0e59c53821abab1625a81244289
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2025-05-03 12:04:04 -0400

    Added project Files

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to th Band Name Generator!\n")
+city = input("What city did you grow up in?\n")
+petName = input("What is the name of your pet?\n")
+print("Your band name could be " + city, petName)
